a823819b2f2835b9ee16d8092af84d8122a02df4
David Stranava <stranava.david@gmail.com>
2022-01-08 18:55:16 +0100

    _app.js rewrite, and index.js cleared:

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react";
+import Menu from "../components/Menu";
+import ButtonBar from "../components/ButtonBar";
+import { Stack, Box } from "@mui/material";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+	return (
+		<SessionProvider session={session}>
+			<Box
+				sx={{
+					maxWidth: "100vw",
+					overflowX: "hidden",
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+				}}
+			>
+				<Menu />
+				<Stack
+					alignItems="center"
+					sx={{ width: "100%", maxWidth: "lg", paddingTop: 2 }}
+				>
+					<ButtonBar />
+					<Component {...papgeProps} />
+				</Stack>
+			</Box>
+		</SessionProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
