244faad864f8ff5b67c4adb1bacc107ddbc1b610
= <=>
2022-01-20 16:59:06 +0100

    created alert context and tweaked alert components, added to _app.js

--- components/page/Alert.js
+++ components/page/Alert.js
@@ -1,11 +1,19 @@
-import { Alert } from '@mui/material';
+import { Alert } from "@mui/material";
 
-const AlertBox = ({ text, severity }) => {
-    return (
-        <Alert severity={severity} sx={{
-            position: 'fixed',
-            bottom: '20px',
-            right: '20px'
-        }}>{text}</Alert>
-    )
-}
+const AlertBox = ({ text, severity, close }) => {
+	return (
+		<Alert
+			severity={severity}
+			sx={{
+				position: "fixed",
+				bottom: "20px",
+				right: "20px",
+			}}
+			onClose={close}
+		>
+			{text}
+		</Alert>
+	);
+};
+
+export default AlertBox;
--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,51 @@
 import { SessionProvider } from "next-auth/react";
+import { useState } from "react";
 // import Menu from "../components/Menu";
 // import ButtonBar from "../components/ButtonBar";
 import { Stack, Box } from "@mui/material";
 
 //components
-import Menu from '../components/page/Menu';
+import AlertContext from "../components/context/AlertContext";
+import AlertBox from "../components/page/Alert";
+import Menu from "../components/page/Menu";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+	const [showAlert, setShowAlert] = useState(true);
+	const [alertText, setAlertText] = useState("");
+	const [alertSeverity, setAlertSeverity] = useState("success");
+
+	const showAlertFunction = (text, severity) => {
+		setShowAlert(true);
+		setAlertText(text);
+		setAlertSeverity(severity);
+	}
+
 	return (
 		<SessionProvider session={session}>
-			<Box
-				sx={{
-					maxWidth: "100vw",
-					overflowX: "hidden",
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-				}}
-			>
-				{/* <Menu /> */}
-				<Menu/>
-				<Stack
-					alignItems="center"
-					sx={{ width: "100%", maxWidth: "lg", paddingTop: 2 }}
+			<AlertContext.Provider value={showAlertFunction}>
+				<Box
+					sx={{
+						maxWidth: "100vw",
+						overflowX: "hidden",
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
 				>
-					{/* <ButtonBar /> */}
-					<Component {...pageProps} />
-				</Stack>
-			</Box>
+					{/* <Menu /> */}
+					<Menu />
+					<Stack
+						alignItems="center"
+						sx={{ width: "100%", maxWidth: "lg", paddingTop: 2 }}
+					>
+						{/* <ButtonBar /> */}
+						<Component {...pageProps} />
+					</Stack>
+					{showAlert && (
+						<AlertBox text={alertText} severity={alertSeverity} close={() => setShowAlert(false)}/>
+					)}
+				</Box>
+			</AlertContext.Provider>
 		</SessionProvider>
 	);
 }
