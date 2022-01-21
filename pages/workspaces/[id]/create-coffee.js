6d4e4578a1be381acbd3509a38e906799efc2b66
= <=>
2022-01-21 19:22:10 +0100

    added more conditional rendering, requests where is protected, created basic state for protected, authorized and editKey

--- pages/workspaces/[id]/create-coffee.js
+++ pages/workspaces/[id]/create-coffee.js
@@ -1,63 +1,103 @@
 // nextjs, react
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 // components
-import CreateCoffee from '../../../components/coffees/CreateCoffee';
+import CreateCoffee from "../../../components/coffees/CreateCoffee";
+import ProtectedCreate from "../../../components/workspace/ProtectedCreate";
 // mui
-import { Box, Typography, Stack, CircularProgress } from '@mui/material';
+import { Box, Typography, Stack, CircularProgress } from "@mui/material";
 
 const createCoffee = () => {
-    const router = useRouter();
-    const workspace_id = router.query.id;
-    const { data: session, status } = useSession();
+	const router = useRouter();
+	const workspace_id = router.query.id;
+	const { data: session, status } = useSession();
     const [isProtected, setProtected] = useState(false);
+    const [authorized, setAuthorized] = useState(false);
+	const [loaded, setLoaded] = useState(false);
+	const [editKey, setEditKey] = useState("");
 
-    const create = (name, description, image, url) => {
-        console.log({ name, description, image, url });
-        axios.post('http://localhost:3001/coffees', {
-            name, description, image, url, user_id: session.id, workspace_id
-        }).then((res) => {
-            if (res.data.message === "coffee created") {
-                router.push(`/workspaces/${workspace_id}`);
-            } else {
-                console.log('there was an error');
-                // render alert box
-            }
-        });
+	const create = (name, description, image, url) => {
+		console.log({ name, description, image, url });
+		axios
+			.post("http://localhost:3001/coffees", {
+				name,
+				description,
+				image,
+				url,
+				user_id: session.id,
+				workspace_id,
+			})
+			.then((res) => {
+				if (res.data.message === "coffee created") {
+					router.push(`/workspaces/${workspace_id}`);
+				} else {
+					console.log("there was an error");
+					// render alert box
+				}
+			});
+    };
+    
+    // edit key
+    const submitEditKey = (editKey) => {
+        console.log('submit edit key', editKey);
     }
 
-    // authentication and data fetching
-    useEffect(() => {
-        if (status === "unauthenticated") {
-            router.push('/');
+	// authentication and data fetching
+	useEffect(() => {
+		if (status === "unauthenticated") {
+			router.push("/");
+		}
+		if (status === "authenticated") {
+			if (!(workspace_id in session.workspaces)) {
+				router.push("/");
+			}
+			axios
+				.get("http://localhost:3001/workspaces/protected", {
+					params: {
+						workspace_id: parseInt(workspace_id),
+						user_id: session.id,
+					},
+				})
+				.then((res) => {
+					setProtected(res.data.protected);
+					setLoaded(true);
+				});
+		}
+		console.log(status);
+	}, [status]);
+
+	if (status === "loading") {
+		return (
+			<Box sx={{ display: "flex" }}>
+				<CircularProgress />
+			</Box>
+		);
+	}
+	if (status === "authenticated") {
+		if (isProtected && loaded && !authorized) {
+			return (
+				<Box sx={{ display: "flex" }}>
+					<ProtectedCreate submit={submitEditKey} />
+				</Box>
+			);
         }
-        if (status === "authenticated") {
-            if (!(parseInt(router.query.id, 10) in session.workspaces)) {
-                router.push('/');
-            }
+        if (loaded && (!isProtected || (isProtected && authorized))) {
+            return (
+				<Box sx={{ display: "flex" }}>
+					<CreateCoffee
+						create={create}
+						id={session.id}
+						workspace_id={workspace_id}
+					/>
+				</Box>
+			);
         }
-        console.log(status);
-    }, [status])
+	}
 
-    if (status === "loading") {
-        return (
-            <Box sx={{display: 'flex'}}>
-                <CircularProgress/>
-            </Box>
-        )
-    }
-    if (status === "authenticated") {
-        return (
-            <Box sx={{ display: 'flex' }}>
-                <CreateCoffee create={create} id={session.id} workspace_id={workspace_id}/>
-            </Box>
-        )
-    }
-
-    return ('Well something is wrong here')
-}
+	return "Well something is wrong here";
+};
 
 export default createCoffee;
