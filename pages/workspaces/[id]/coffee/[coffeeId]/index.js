238fa6efd31428844c740fd5e7da1ccd4f1c58ca
= <=>
2022-01-23 14:35:01 +0100

    something i don't remember

---
+++ pages/workspaces/[id]/coffee/[coffeeId]/index.js
@@ -0,0 +1,64 @@
+//next js stuff
+import Link from "next/link";
+import { useRouter } from 'next/router';
+
+import { useSession } from "next-auth/react";
+
+import CoffeesContext from "../../../../components/context/CoffeesContext";
+
+//components
+import Coffee from "../../../../components/coffees/Coffee";
+
+// mui
+import { Box, CircularProgress } from "@mui/material";
+
+const coffee = () => {
+	const { data: session, status } = useSession();
+	const router = useRouter();
+	const currentCoffeeId = router.query.coffeeId;
+	// SSG fast return 
+	if (status === "loading") {
+		return (
+			<Box>
+				<CircularProgress />
+			</Box>
+		);
+	}
+
+	// user is logged in, not in this workspace
+	if (
+		status === "authenticated" &&
+		!session.workspaces.includes(workspace_id)
+	) {
+		return (
+			<Box>
+				<Typography>
+					You have to first
+					{/* <Link
+						href={{ pathname: "/add", query: { workspaceId } }}
+					>
+						log in
+					</Link> */}
+					login
+					to use this workspace
+				</Typography>
+			</Box>
+		);
+	}
+
+	// user is IN this workspace
+	return (
+		<CoffeesContext.Provider
+			value={{
+				coffee,
+				currentCoffee: { id: currentCoffeeId },
+				setCurrent,
+				deleteCoffee,
+			}}
+		>
+			<Coffee coffee={coffee} />
+		</CoffeesContext.Provider>
+	);
+};
+
+export default coffee;
