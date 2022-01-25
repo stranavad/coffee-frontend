21d8fb65abf839fc345bd7b14a655c6c796a39af
= <=>
2022-01-25 10:17:40 +0100

    basic structure and styling of /"

--- pages/index.js
+++ pages/index.js
@@ -2,48 +2,45 @@
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 //components
-import AddCreateButton from '../components/workspace/AddCreateButton';
+import AddCreateButton from "../components/workspace/AddCreateButton";
 
 // material ui
-import { Box, CircularProgress, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function Home() {
-	const {status } = useSession();
+	const { status } = useSession();
 
-	// user is logged in
-	if (status === "authenticated") {
-		return (
-			<Box sx={{ display: "flex", flexDirection: "column" }}>
-				<Box>
-					<Typography>Check out your <Link href="/workspaces">Workspaces</Link></Typography>
-					<AddCreateButton/>
-				</Box>
-			</Box>
-		);
-	}
-
-	// the user state is yet to be determined
-	if (status === "loading") {
-		return (
-			<Box sx={{ display: "flex" }}>
-				<CircularProgress />
-			</Box>
-		);	
-	}
-
-	// users isn't logged in
-	if (status === "unauthenticated") {
-		return (
-			<Box sx={{ display: 'flex' }}>
-				<Typography>You need to first login to use this app</Typography>
-			</Box>
-		)
-	}
+	// // user is logged in
+	// if (status === "authenticated") {
+	// 	return (
+	// 		<Box sx={{ display: "flex", flexDirection: "column" }}>
+	// 			<Box>
+	// 				<Typography>Check out your <Link href="/workspaces">Workspaces</Link></Typography>
+	// 				<AddCreateButton/>
+	// 			</Box>
+	// 		</Box>
+	// 	);
+	// }
+	// // users isn't logged in
+	// if (status === "unauthenticated") {
+	// 	return (
+	// 		<Box sx={{ display: 'flex' }}>
+	// 			<Typography>You need to first login to use this app</Typography>
+	// 		</Box>
+	// 	)
+	// }
 	// some weird case
 	return (
-		<Box sx={{ display: 'flex' }}>
-			<Typography>There was some error that needs to be reported</Typography>
+		<Box sx={{ display: "flex", flexDirection: 'column' }}>
+			<Typography variant="h3" sx={{marginBottom: 2}}>
+				Welcome to Coffee
+			</Typography>
+			{status === "authenticated" && (
+				<>
+					<Typography variant="h6"><Link href="/workspaces">Check out your workspaces</Link></Typography>
+					<AddCreateButton />
+				</>
+			)}
 		</Box>
-	)
-
+	);
 }
