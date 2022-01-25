8b4f6b8db0bcfeb46addd70b5d1d1c7037d5a6cb
= <=>
2022-01-25 10:55:23 +0100

    added basic leave workspace functionality

--- components/workspace/Workspaces.js
+++ components/workspace/Workspaces.js
@@ -1,17 +1,43 @@
 //nextjs, react
+import { useSession } from "next-auth/react";
+import { useRouter } from 'next/router';
+import axios from "axios";
 //components
 import WorkspaceCard from "./WorkspaceCard";
 //mui
 import { Grid, Typography } from "@mui/material";
 
 const Workspaces = ({ workspaces }) => {
+	const { data: session } = useSession();
+	const router = useRouter();
+	const leaveWorkspace = (workspaceId) => {
+		axios
+			.delete("http://localhost:3001/user/workspace", {
+				params: {
+					userId: session.id,
+					workspaceId
+				}
+			})
+			.then((res) => {
+				if (res.data.message === "removed") {
+					router.reload();
+				} else {
+					console.log("there was some problem");
+				}
+			});
+		console.log("leave workspace" + workspaceId);
+	};
+
 	return (
 		<>
 			<Typography variant="h4">Your workspaces</Typography>
 			<Grid container spacing={4} sx={{ paddingTop: 3 }}>
 				{workspaces.map((workspace) => (
 					<Grid item key={workspace.id}>
-						<WorkspaceCard workspace={workspace} />
+						<WorkspaceCard
+							workspace={workspace}
+							leaveWorkspace={leaveWorkspace}
+						/>
 					</Grid>
 				))}
 			</Grid>
